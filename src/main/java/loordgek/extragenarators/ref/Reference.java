73a06db03faf2fe332b28392d46c60fdcf78309c
loordgek <stefanhempenius@hotmail.com>
2017-02-08 22:19:36 +0100

    help

--- src/main/java/loordgek/extragenarators/ref/Reference.java
+++ src/main/java/loordgek/extragenarators/ref/Reference.java
@@ -1,5 +1,7 @@
 package loordgek.extragenarators.ref;
 
+import net.minecraft.launchwrapper.Launch;
+
 public class Reference {
     public static class MODINFO {
         public static final String MOD_ID = "extragenarators";
@@ -7,6 +9,7 @@ public class Reference {
         public static final String VERSION = "1.10.2-1.0";
         public static final String CLIENT_PROXY_CLASS = "loordgek.extragenarators.proxy.ClientProxy";
         public static final String SERVER_PROXY_CLASS = "loordgek.extragenarators.proxy.ServerProxy";
+        public static final boolean devmode = (Boolean) Launch.blackboard.get("fml.deobfuscatedEnvironment");
     }
 
     public static class BLOCKS {
